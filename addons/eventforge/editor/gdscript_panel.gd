2d8d56a6e4a6a16e8cb37e9adebd8008e1723249
Salman Shurie <spottishgames@gmail.com>
2026-05-16 00:19:29 +0100

    fix: use Godot 4.5 CodeEdit API for GDScript panel

--- addons/eventforge/editor/gdscript_panel.gd
+++ addons/eventforge/editor/gdscript_panel.gd
@@ -4,12 +4,12 @@ extends CodeEdit
 class_name GDScriptPanel
 
 func _ready() -> void:
-    editable = false
-    draw_line_numbers = true
-    line_wrapping_mode = TextEdit.LINE_WRAPPING_NONE
+	editable = false
+	set_draw_line_numbers(true)
+	set_line_wrapping_mode(TextEdit.LINE_WRAPPING_NONE)
 
 ## Sets the displayed source text.
 func set_source(code: String) -> void:
-    text = code
-    set_caret_line(0)
-    set_caret_column(0)
+	text = code
+	set_caret_line(0)
+	set_caret_column(0)
